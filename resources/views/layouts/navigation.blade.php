fa1e80991a3275f6c2fb9c03ef19077e41b010b6
Giovane <giovanelr@gmail.com>
2023-08-08 10:36:33 -0300

    test: adicionado testes de crud produto

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,11 @@
                         {{ __('Products') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('customers')" :active="request()->routeIs('customers')">
+                        {{ __('Customers') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
