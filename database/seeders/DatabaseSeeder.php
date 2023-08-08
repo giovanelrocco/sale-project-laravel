ad576a3998b5b5864f51f05e46f96ae461677596
Giovane <giovanelr@gmail.com>
2023-08-08 10:40:18 -0300

    fix: ajustado seeder do banco

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
-
         \App\Models\User::factory()->create([
             'name' => 'Giovane',
             'email' => 'giovane@test.com',
-            'password' => 'test1234'
+            'password' => 'Test1234'
         ]);
     }
 }
